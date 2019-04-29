fcd9cde0348c0b7137aabc5e9311680e631595f5
svntax <endless.answers@gmail.com>
2019-04-29 16:39:27 -0400

    Add main menu, arena, and game over themes.

--- Scripts/main_menu_ui_controls.gd
+++ Scripts/main_menu_ui_controls.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _ready():
     Globals.resetUpgrades()
+    SoundHandler.mainMenuTheme.play()
 
 func _on_Start_pressed():
+    SoundHandler.mainMenuTheme.stop()
     get_tree().change_scene("res://Scenes/gameplay.tscn")
